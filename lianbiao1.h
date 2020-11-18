0140c45fa440c9de5c206e34d64957b8879351f0
wanzujun <1714660351@qq.com>
2020-11-18 21:57:34 +0800

    first commit

---
+++ lianbiao1.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef int DataType;
+ 
+typedef struct node_{
+         DataType data;
+          struct node_ *next;
+  } Node;
+
+ 
+ void initList(Node *);
+ int Size(Node *);
+ void Insert(Node *, int, DataType);
+ void Delete(Node *, DataType);
+ int Empty(Node *);
+ DataType GetData(Node *, DataType);
+ Node *Find(Node *, DataType);
+ void Print(Node *);
+ void ClearList(Node *);
